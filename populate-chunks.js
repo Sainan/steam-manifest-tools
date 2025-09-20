4b2d4c8b4c3ca4bd1ee598538da618abd60e65cb
Sainan <63328889+Sainan@users.noreply.github.com>
2025-09-20 21:12:05 +0200

    Remove unused import

--- populate-chunks.js
+++ populate-chunks.js
@@ -3,7 +3,6 @@ const fs = require("fs");
 const fsPromises = require("fs/promises");
 const path = require("path");
 const AdmZip = require("adm-zip");
-const CdnCompression = require("steam-user/components/cdn_compression");
 const SteamCrypto = require("@doctormckay/steam-crypto");
 
 const sha1 = (data) => crypto.createHash("sha1").update(data).digest("hex");
