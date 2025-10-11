f25bda283d72d63e0f4c4a6ea92d1830575301f6
Sainan <63328889+Sainan@users.noreply.github.com>
2025-10-11 20:32:00 +0200

    Initial move toward being a library, starting with populate-chunks

---
+++ include/lib.js
@@ -0,0 +1,45 @@
+const crypto = require("crypto");
+const fs = require("fs");
+const fsPromises = require("fs/promises");
+const path = require("path");
+const AdmZip = require("adm-zip");
+const SteamCrypto = require("@doctormckay/steam-crypto");
+
+const sha1 = (data) => crypto.createHash("sha1").update(data).digest("hex");
+
+const compress = (buf) => {
+	const zip = new AdmZip();
+	zip.addFile("z", buf);
+	return zip.toBuffer();
+};
+
+// https://stackoverflow.com/a/45130990
+const getFiles = async (dir) => {
+	const dirents = await fsPromises.readdir(dir, { withFileTypes: true });
+	const files = await Promise.all(dirents.map((dirent) => {
+		const res = path.resolve(dir, dirent.name);
+		return dirent.isDirectory() ? getFiles(res) : res;
+	}));
+	return Array.prototype.concat(...files);
+};
+
+module.exports = {
+	populateChunks: async (depotId, depotKey, installDir) => {
+		depotKey = Buffer.from(depotKey, "hex");
+		const files = await getFiles(installDir);
+		fs.mkdirSync(`depot/${depotId}/chunk`, { recursive: true });
+		for (const file of files) {
+			const data = await fsPromises.readFile(file);
+			for (let offset = 0; offset < data.length; offset += 1048576) {
+				const end = Math.min(offset + 1048576, data.length);
+				let chunk = data.subarray(offset, end);
+				const sha = sha1(chunk);
+				if (!fs.existsSync(`depot/${depotId}/chunk/${sha}`)) {
+					chunk = compress(chunk);
+					chunk = SteamCrypto.symmetricEncrypt(chunk, depotKey);
+					await fsPromises.writeFile(`depot/${depotId}/chunk/${sha}`, chunk);
+				}
+			}
+		}
+	},
+};
