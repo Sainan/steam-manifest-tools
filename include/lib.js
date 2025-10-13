cf25c07a08b21a9c9f196600aece77bb053fcd66
Sainan <63328889+Sainan@users.noreply.github.com>
2025-10-13 09:49:30 +0200

    Add install to lib

--- include/lib.js
+++ include/lib.js
@@ -3,26 +3,9 @@ const fs = require("fs");
 const fsPromises = require("fs/promises");
 const path = require("path");
 const AdmZip = require("adm-zip");
+const CdnCompression = require("steam-user/components/cdn_compression");
 const ContentManifest = require("steam-user/components/content_manifest");
 const SteamCrypto = require("@doctormckay/steam-crypto");
-
-const sha1 = (data) => crypto.createHash("sha1").update(data).digest("hex");
-
-const compress = (buf) => {
-	const zip = new AdmZip();
-	zip.addFile("z", buf);
-	return zip.toBuffer();
-};
-
-// https://stackoverflow.com/a/45130990
-const getFiles = async (dir) => {
-	const dirents = await fsPromises.readdir(dir, { withFileTypes: true });
-	const files = await Promise.all(dirents.map((dirent) => {
-		const res = path.resolve(dir, dirent.name);
-		return dirent.isDirectory() ? getFiles(res) : res;
-	}));
-	return Array.prototype.concat(...files);
-};
 
 const DEFAULT_HOSTS = [
 	"https://cache8-sto1.steamcontent.com",
@@ -47,8 +30,65 @@ const DEFAULT_HOSTS = [
 	"https://google2.cdn.steampipe.steamcontent.com",
 ];
 
+const sha1 = (data) => crypto.createHash("sha1").update(data).digest("hex");
+
+const sha1file = async (file) => {
+	const st = crypto.createHash("sha1");
+	const readStream = fs.createReadStream(file, { highWaterMark: 1048576 });
+	for await (const chunk of readStream) {
+		st.update(chunk);
+	}
+	return st.digest("hex");
+};
+
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+const compress = (buf) => {
+	const zip = new AdmZip();
+	zip.addFile("z", buf);
+	return zip.toBuffer();
+};
+
+// https://stackoverflow.com/a/45130990
+const getFiles = async (dir) => {
+	const dirents = await fsPromises.readdir(dir, { withFileTypes: true });
+	const files = await Promise.all(dirents.map((dirent) => {
+		const res = path.resolve(dir, dirent.name);
+		return dirent.isDirectory() ? getFiles(res) : res;
+	}));
+	return Array.prototype.concat(...files);
+};
+
+const getChunk = async (depotId, depotKey, hash) => {
+	while (true) {
+		try {
+			let data = await fsPromises.readFile(`depot/${depotId}/chunk/${hash}`);
+			data = SteamCrypto.symmetricDecrypt(data, depotKey);
+			data = await CdnCompression.unzip(data);
+			if (sha1(data) != hash) {
+				throw new Error(`depot/${depotId}/chunk/${hash} does not match the expected hash`);
+			}
+			return data;
+		} catch (e) {
+			if (e.code == "ENOENT") {
+				throw new Error(`depot/${depotId}/chunk/${hash} is missing`);
+			}
+			if (e.code != "EMFILE") {
+				//console.log(`Error reading depot/${depotId}/chunk/${hash}`);
+				throw e;
+			}
+			await sleep(500);
+		}
+	}
+};
+
 module.exports = {
+	DEFAULT_HOSTS,
+	sha1,
+	sha1file,
+	compress,
 	getFiles,
+	getChunk,
 	populateChunks: async (manifest, depotKey, installDir, onProgress) => {
 		if (manifest.filenames_encrypted) {
 			ContentManifest.decryptFilenames(manifest, depotKey);
@@ -84,7 +124,6 @@ module.exports = {
 			++file_i;
 		}
 	},
-	DEFAULT_HOSTS,
 	downloadChunks: (manifest, onStartDownloading, onStartDownload, onFinishDownload, onErroredDownload, hosts) => {
 		hosts ??= DEFAULT_HOSTS;
 		return new Promise(resolve => {
@@ -145,5 +184,34 @@ module.exports = {
 			};
 			loop();
 		});
+	},
+	install: async (manifest, depotKey, installDir, onFileWritten) => {
+		ContentManifest.decryptFilenames(manifest, depotKey);
+		installDir ??= `install/${manifest.depot_id}/${manifest.gid_manifest}`;
+		fs.mkdirSync(installDir, { recursive: true });
+		for (const file of manifest.files) {
+			if (file.flags & 64) {
+				continue;
+			}
+			const exists = fs.existsSync(path.join(installDir, file.filename));
+			if (!exists || await sha1file(path.join(installDir, file.filename)) != file.sha_content) {
+				for (const chunk of file.chunks) {
+					chunk.offset = parseInt(chunk.offset);
+				}
+				file.chunks.sort((a, b) => a.offset - b.offset);
+
+				const chunkPromises = [];
+				for (const chunk of file.chunks) {
+					chunkPromises.push(getChunk(manifest.depot_id, depotKey, chunk.sha));
+				}
+				const chunks = await Promise.all(chunkPromises);
+				const data = Buffer.concat(chunks);
+				await fsPromises.mkdir(path.dirname(path.join(installDir, file.filename)), { recursive: true });
+				await fsPromises.writeFile(path.join(installDir, file.filename), data);
+				if (onFileWritten) {
+					onFileWritten(file.filename, exists);
+				}
+			}
+		}
 	},
 };
--- install.js
+++ install.js
@@ -5,85 +5,17 @@ if (!manifestFile) {
 	process.exit(1);
 }
 
-const crypto = require("crypto");
 const fs = require("fs");
-const fsPromises = require("fs/promises");
-const path = require("path");
-const CdnCompression = require("steam-user/components/cdn_compression");
 const ContentManifest = require("steam-user/components/content_manifest");
-const SteamCrypto = require("@doctormckay/steam-crypto");
+const { install }  = require(".");
 
-const sha1 = (data) => crypto.createHash("sha1").update(data).digest("hex");
-
-const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-
-const chunksInMemory = {};
-const getChunk = async (depotId, depotKey, hash) => {
-	if (!chunksInMemory[hash]) {
-		while (true) {
-			try {
-				let data = await fsPromises.readFile(`depot/${depotId}/chunk/${hash}`);
-				data = SteamCrypto.symmetricDecrypt(data, depotKey);
-				data = await CdnCompression.unzip(data);
-				if (sha1(data) != hash) {
-					console.log(`Uh oh, depot/${depotId}/chunk/${hash} does not match the expected hash`);
-				}
-				chunksInMemory[hash] = data;
-				break;
-			} catch (e) {
-				if (e.code == "ENOENT") {
-					console.log(`Uh oh, depot/${depotId}/chunk/${hash} is missing`);
-					chunksInMemory[hash] = new Uint8Array();
-					break;
-				}
-				if (e.code != "EMFILE") {
-					console.log(`Error reading depot/${depotId}/chunk/${hash}`);
-					throw e;
-				}
-				await sleep(500);
-			}
-		}
-	}
-	return chunksInMemory[hash];
-};
-
-const data = fs.readFileSync(manifestFile);
 (async () => {
-	const manifest = ContentManifest.parse(data);
+	const manifest = ContentManifest.parse(fs.readFileSync(manifestFile));
 	if (!depotKey) {
 		depotKey = await require("./include/fetch-depot-key.js")(manifest.depot_id);
 	}
 	depotKey = Buffer.from(depotKey, "hex");
-	ContentManifest.decryptFilenames(manifest, depotKey);
-	fs.mkdirSync(`install/${manifest.depot_id}/${manifest.gid_manifest}`, { recursive: true });
-	for (const file of manifest.files) {
-		if (file.flags & 64) {
-			continue;
-		}
-		if (!fs.existsSync(`install/${manifest.depot_id}/${manifest.gid_manifest}/${file.filename}`)) {
-			for (const chunk of file.chunks) {
-				chunk.offset = parseInt(chunk.offset);
-			}
-			file.chunks.sort((a, b) => a.offset - b.offset);
-			const chunkPromises = [];
-			for (const chunk of file.chunks) {
-				chunkPromises.push(getChunk(manifest.depot_id, depotKey, chunk.sha));
-			}
-			const chunks = await Promise.all(chunkPromises);
-			(async () => {
-				const st = crypto.createHash("sha1");
-				for (const chunk of chunks) {
-					st.update(chunk);
-				}
-				const data = Buffer.concat(chunks);
-				await fsPromises.mkdir(path.dirname(`install/${manifest.depot_id}/${manifest.gid_manifest}/${file.filename}`), { recursive: true });
-				await fsPromises.writeFile(`install/${manifest.depot_id}/${manifest.gid_manifest}/${file.filename}`, data);
-				if (st.digest("hex") == file.sha_content) {
-					console.log(`install/${manifest.depot_id}/${manifest.gid_manifest}/${file.filename}: Created`);
-				} else {
-					console.log(`install/${manifest.depot_id}/${manifest.gid_manifest}/${file.filename}: Created; HASH MISMATCHES`);
-				}
-			})();
-		}
-	}
+	await install(manifest, depotKey, undefined, (file, existed) => {
+		console.log(`${existed ? "Repaired" : "Created"} ${file}`);
+	});
 })();
